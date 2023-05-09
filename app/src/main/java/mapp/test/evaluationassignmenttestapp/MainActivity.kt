aeb4090fcbafdc00dc0dc92b9e50bf75f5dbbe9f
panterra-000 <raximjanovdavronbek@gmail.com>
2023-05-09 20:00:43 +0500

    dddd

---
+++ app/src/main/java/mapp/test/evaluationassignmenttestapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package mapp.test.evaluationassignmenttestapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import mapp.test.evaluationassignmenttestapp.ui.theme.EvaluationAssignmentTestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EvaluationAssignmentTestAppTheme {
+
+            }
+        }
+    }
+}
